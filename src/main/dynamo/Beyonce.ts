ab5134b97f894e27d9c630222d10458f46f73d00
Bryan Gaston <1993853+gbryan@users.noreply.github.com>
2022-08-12 18:11:57 -0700

    feat: support uniqueness check on PUT (#79)

--- src/main/dynamo/Beyonce.ts
+++ src/main/dynamo/Beyonce.ts
@@ -159,14 +159,24 @@ export class Beyonce {
     return await batchWriteItems({ table: this.table, client: this.client, putItems: maybeEncryptedItems, deleteItems })
   }
 
-  /** Perform N Dynamo operations in an atomic transaction */
+  /**
+   * Perform N Dynamo operations in an atomic transaction.
+   *
+   * @param params.putItems Set `failIfNotUnique` to true on a record within putItems to cancel the transaction if a
+   *  record with the same partition key and sort key (if applicable) already exists.
+   */
   async batchWriteWithTransaction<T extends TaggedModel>(params: {
     clientRequestToken?: string
-    putItems?: T[]
+    putItems?: (T & { failIfNotUnique?: boolean })[]
     deleteItems?: PartitionAndSortKey<T>[]
   }): Promise<void> {
     const { clientRequestToken, putItems = [], deleteItems = [] } = params
-    const maybeEncryptedPutPromises = putItems.map(async (item) => this.maybeEncryptItem(item))
+    const maybeEncryptedPutPromises = putItems.map(async ({ failIfNotUnique, ...item }) => {
+      return {
+        item: await this.maybeEncryptItem(item as T),
+        failIfNotUnique
+      }
+    })
     const maybeEncryptedItems = await Promise.all(maybeEncryptedPutPromises)
     await transactWriteItems({
       table: this.table,
