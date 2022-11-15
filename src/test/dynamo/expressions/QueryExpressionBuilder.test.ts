ed066024be95d4ff324134c4efbb988829a057b9
skarode@poppulo.com <skarode@poppulo.com>
2022-11-15 21:35:02 +0000

    add support for BETWEEN clause in expression

--- src/test/dynamo/expressions/QueryExpressionBuilder.test.ts
+++ src/test/dynamo/expressions/QueryExpressionBuilder.test.ts
@@ -86,6 +86,22 @@ describe("ExpressionBuilder basic clauses", () => {
       }
     })
   })
+
+  it("should support BETWEEN clause", () => {
+    const result = exp().where("name", "=", "Bob Marley").andBetween("divaRating", "1", "3").build()
+    expect(result).toEqual({
+      "attributeNames": {
+        "#divaRating": "divaRating",
+        "#name": "name"
+      },
+      "attributeValues": {
+        ":v1": "Bob Marley",
+        ":v2": "1",
+        ":v3": "3"
+      },
+      "expression": "#name = :v1 AND #divaRating BETWEEN :v2 AND :v3"
+    })
+  });
 })
 
 describe("ExpressionBuilder attribute_exists and attribute_not_exists clauses", () => {
