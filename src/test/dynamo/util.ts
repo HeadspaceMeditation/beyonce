75a35bb546fb47fa3daf7150dc8877b5c719bd1a
Douglas Eichelberger <dduugg@gmail.com>
2024-04-25 11:24:08 -0700

    Update util

--- src/test/dynamo/util.ts
+++ src/test/dynamo/util.ts
@@ -6,7 +6,7 @@ import { Beyonce } from "../../main/dynamo/Beyonce"
 import { Song, SongModel, table } from "./models"
 
 export const port = 8000
-const isRunningOnCI = process.env.CI_BUILD_ID !== undefined
+const isRunningOnCI = process.env.CI
 // When running in the CI env, we run Dynamo in a Docker container. And the host must match the service name defined in codeship-services.yml
 // see https://documentation.codeship.com/pro/builds-and-configuration/services/#container-networking
 const endpoint = isRunningOnCI ? `http://dynamodb:${port}` : `http://localhost:${port}`
